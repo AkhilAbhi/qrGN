fa66eaa2bcf06e4bdc370ce54b4872fda0f0b2df
AkhilAbhi <66011589+AkhilAbhi@users.noreply.github.com>
2023-01-28 16:39:29 +0530

    Some update
    Some update

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var button = document.querySelector('.submit');
 var img = document.querySelector('#img');
 var h2 = document.querySelector('#h2');
 var imgdiv = document.querySelector('#imgDiv');
-
+var Waring= document.querySelector('.Waring');
+var box= document.querySelector('.box');
 
 button.onclick = function(){
  
@@ -11,6 +12,9 @@ button.onclick = function(){
   var s=input.value.length;
   if (s>=1) {
   //alert('hay');
+  input.style.border='2px solid #03fcd3';
+  box.style.border='2px solid lawngreen';
+  Waring.style.display='none';
   button.innerHTML='Generating..'
   img.src='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='+input.value;
   img.onload=function(){
@@ -18,8 +22,11 @@ button.onclick = function(){
     imgdiv.style.display='block';
   }
 }else{
-  h2.innerHTML='Enter Text Or Url ';
+  Waring.style.display='block';
+  box.style.border='2px solid red';
+  input.style.border='2px solid red';
   imgdiv.style.display='none';
+  
 }
 
 
